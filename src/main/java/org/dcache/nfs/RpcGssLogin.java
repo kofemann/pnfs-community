316d2e4787363b555467c0f9cee0f0c6fbc86c5c
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2018-08-14 16:19:19 +0200

    gss: preserve kerberos principal on login

--- src/main/java/org/dcache/nfs/RpcGssLogin.java
+++ src/main/java/org/dcache/nfs/RpcGssLogin.java
@@ -1,19 +1,32 @@
 package org.dcache.nfs;
 
 import javax.security.auth.Subject;
+import javax.security.auth.kerberos.KerberosPrincipal;
 import org.dcache.auth.GidPrincipal;
 import org.dcache.auth.UidPrincipal;
 import org.dcache.oncrpc4j.rpc.RpcLoginService;
 import org.dcache.oncrpc4j.rpc.RpcTransport;
 import org.ietf.jgss.GSSContext;
+import org.ietf.jgss.GSSException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class RpcGssLogin implements RpcLoginService {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(RpcGssLogin.class);
 
     @Override
     public Subject login(RpcTransport transport, GSSContext gssContext) {
         Subject s = new Subject();
         s.getPrincipals().add(new UidPrincipal(10006));
         s.getPrincipals().add(new GidPrincipal(1000, true));
+
+        // preserve original Kerberos principal, if possible ;)
+        try {
+            s.getPrincipals().add(new KerberosPrincipal(gssContext.getSrcName().toString()));
+        } catch (GSSException e) {
+            LOGGER.warn("Failed to get source pringipal: ", e.getMessage());
+        }
         return s;
     }
 }
