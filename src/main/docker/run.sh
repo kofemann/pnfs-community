3cd062a122e2d22ccbe049d3de9a88165df493d6
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2018-10-31 17:11:18 +0100

    docker: make a single image for ds and mds

--- src/main/docker/run.sh
+++ src/main/docker/run.sh
@@ -1,6 +1,24 @@
 #!/bin/sh
 
+JAVA_ARGS=
+
+case $1 in
+
+  mds)
+  echo "Staring MDS"
+  ;;
+  ds)
+	PORT=`/agent-client.py`
+	JAVA_ARGS=-DPNFS_DS_ADDRESS="${LOCALADDRESS}:${PORT}"
+	echo "Staring DS on ${LOCALADDRESS}:${PORT}"
+  ;;
+  *)
+  echo "Invalid option " $1
+  exit 1
+  ;;
+esac
+
 cd /opt/pnfs
-/usr/bin/java -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap \
-	-server ${JAVA_OPT} \
+/usr/bin/java -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -server \
+	${JAVA_OPT} ${JAVA_ARGS} \
 	-cp "/opt/pnfs/jars/*" org.dcache.nfs.Main svc.xml
