104038e7716702de4a01ca1618fa8117a93a9743
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2019-05-14 15:28:19 +0200

    docker: unify run.sh

--- src/main/docker/run.sh
+++ src/main/docker/run.sh
@@ -1,25 +1,7 @@
 #!/bin/sh
 
-JAVA_ARGS=
-
-case $1 in
-
-  mds)
-  echo "Staring MDS"
-  ;;
-  ds)
-  JAVA_ARGS=-DPNFS_DS_ADDRESS="${LOCALADDRESS}"
-  echo "Staring DS"
-  ;;
-  sh)
-  exec /bin/sh
-  ;;
-  *)
-  echo "Invalid option " $1
-  exit 1
-  ;;
-esac
+JAVA_ARGS=-DPNFS_DS_ADDRESS="${LOCALADDRESS}"
 
 exec /usr/bin/java -server \
 	${JAVA_OPT} ${JAVA_ARGS} \
-	-cp "/pnfs/jars/*" org.dcache.nfs.Main $1
+	-cp "/pnfs/jars/*" org.dcache.nfs.Main "$@"
